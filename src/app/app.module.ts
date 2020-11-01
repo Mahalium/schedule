d420b6cdd30bfb7f470f66cda59050a6eb4b6e87
Tuyen <michael.ross.mckay@gmail.com>
2020-11-01 14:50:40 -0500

    add mat tabs for week and month

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { CalendarComponent } from './calendar/calendar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns'
+import {MatTabsModule} from '@angular/material/tabs';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns'
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory
-    })
+    }),
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
